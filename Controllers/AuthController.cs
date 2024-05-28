3939df79eb9651af5313d1ea5f30a30e8e99506e
Oranuch T <oranucht@bangchak.co.th>
2024-05-28 14:08:53 +0700

    release v3.0.0

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -36,7 +36,7 @@ public class AuthController : ControllerBase
     [HttpGet]
     public IActionResult Home()
     {
-        return Ok(new { message = "Hello Auth Service 2.0.2" });
+        return Ok(new { message = "Hello Auth Service 3.0.0" });
     }
 
     // localhost:port/api/v1/Auth/Register
